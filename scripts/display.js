84d83c590133fd347bf0c80051cff4ec53cd92ad
Mohamed BAKHCHA <simonbakhcha@gmail.com>
2024-12-30 19:05:48 +0100

    getParams

--- scripts/display.js
+++ scripts/display.js
@@ -4,12 +4,21 @@ import { data } from "scripts/fetch.js";
 
 export const displayHeroes = () => {
      const heroesOnPage = document.getElementsByTagName(tr)
-     const arr  = getParams() // gives search data and number to display    and page 
+     obj = getParams() // gives search data and number to display    and page 
+
+
+
 
 } 
 
 
 
 const getParams = () => {
-     return Array.from(document.body.querySelectorAll('data-input'))
-}
+     const elements = Array.from(document.querySelectorAll('.data-input'));
+ 
+     const searchResult = elements[0]?.textContent.trim() || ''; 
+     const heroesCount = parseInt(elements[1]?.textContent.trim()) || 20; 
+     const pageNumber = parseInt(elements[2]?.textContent.trim()) || 1; 
+ 
+     return { searchResult, heroesCount, pageNumber };
+ };
