3c1fe9ab9659032cb730b9904784cad07e58b745
Mohamed BAKHCHA <simonbakhcha@gmail.com>
2024-12-31 16:35:41 +0100

    display started working

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,47 @@
 let data = []
 let select = document.querySelector('select')
 let search = document.querySelector('input')
+let tbody = document.querySelector('tbody')
+
 // let mok = document.querySelector('select')
 // mok.addEventListener('click', displayHeroes)
 
 async function init() {
     const response = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
-    
+    //err
     data = await response.json()
     displayHeroes()
-    
+
 }
 const displayHeroes = (page = 1) => {
     const heroesOnPage = document.getElementsByTagName('tr')
-    const obj = getParams()      
-    console.log(obj);
-      
-    for (let i = 0; i < obj.heroesCount; i++) {
-        
-    }
+    const obj = getParams()
+    const start = (obj.heroesCount * obj.pageNumber) - obj.heroesCount
+    const end = obj.heroesCount * obj.pageNumber
+    console.log(data[0].powerstats);
     
+    for (let i = start; i < end; i++) {
+        const tr = document.createElement('tr') 
+        tr.innerHTML = `
+        <td><img src="${data[i].images.xs}"><src></td>
+        <td>${data[i].name}</td>
+        <td>${data[i].biography.fullName }</td>
+        <td>${data[i].name}</td>
+        <td>${data[i].appearance.race}</td>
+        <td>${data[i].appearance.gender}</td>
+        <td>${data[i].appearance.height}</td>
+        <td>${data[i].appearance.weight}</td>
+        <td>${data[i].biography.placeOfBirth}</td>
+        <td>${data[i].biography.alignment}</td>`
+        
+        tbody.appendChild(tr)
+
+    }
+
 }
 
 select.addEventListener('click', displayHeroes)
-search.addEventListener('keyup', displayHeroes)
+search.addEventListener('keydown', displayHeroes)
 
 // mok.addEventListener('click', (num) => {
 //   v = parseInt(num.target.value)
@@ -32,13 +50,12 @@ search.addEventListener('keyup', displayHeroes)
 const getParams = () => {
     const elements = Array.from(document.querySelectorAll('[data-input]'));
     // console.log(elements);
-    
-    
-    const searchResult = elements[0]?.value.trim() || '';
-    const heroesCount = parseInt(elements[1].value) || data.length;
-    const pageNumber = parseInt(elements[2]?.value) || 1;
 
-    return { searchResult, heroesCount, pageNumber };
+
+    const searchResult = elements[0]?.value.trim() || ''
+    const heroesCount = parseInt(elements[1].value) || data.length
+    const pageNumber = parseInt(elements[2]?.value) || 1
+
+    return { searchResult, heroesCount, pageNumber }
 }
-// console.log(data);
 addEventListener('DOMContentLoaded', init)
